9cbf2714a4ec4db53037044291df16a42167a650
Adaz SirAdaz <mrxptdrgame@gmail.com>
2024-09-11 16:25:13 +0200

    first commit

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Author;
+use App\Repository\AuthorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class AuthorController extends AbstractController
+{
+    #[Route('/api/authors', name: 'authors', methods: ['GET'])]
+    public function getAllAuthors(AuthorRepository $authorRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $authorList = $authorRepository->findAll();
+        $jsonAuthorList = $serializer->serialize($authorList, 'json', ['groups' => 'getBooks']);
+        return new JsonResponse($jsonAuthorList, Response::HTTP_OK, [], true);
+    }
+    #[Route('/api/authors/{id}', name: 'detailAuthor', methods: ['GET'])]
+    public function getDetailAuthor(Author $author, SerializerInterface
+    $serializer): JsonResponse
+    {
+        $jsonAuthor = $serializer->serialize($author, 'json', ['groups'=> 'getBooks']);
+        return new JsonResponse(
+            $jsonAuthor,
+            Response::HTTP_OK,
+            [],
+            true
+        );
+    }
+}
